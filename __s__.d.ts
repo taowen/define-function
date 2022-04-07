89158b1c2457a557a14199735a250a066533e372
Tao Wen <taowen@chengfayun.com>
2022-04-07 14:24:54 +0800

    allow prop to be number or symbol

--- __s__.d.ts
+++ __s__.d.ts
@@ -2,8 +2,8 @@ type sandboxFunctionToken = { __brand: 'sandboxFunctionToken' }
 declare const __s__: {
     wrapSandboxFunction(f: Function, extra?: { once?: boolean }): sandboxFunctionToken;
     deleteSandboxFunction(token: sandboxFunctionToken);
-    getProp(hostObj: any, prop: string): any;
-    setProp(hostObj: any, prop: string, value: any): void;
+    getProp(hostObj: any, prop: string | number | symbol): any;
+    setProp(hostObj: any, prop: string | number | symbol, value: any): void;
     callMethod(hostObj: any, method: string, ...args: any[]): any;
     deleteHostObject(hostObj: any): void;
 };
